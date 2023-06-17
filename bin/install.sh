fefe4ce8ca0ddf8ab1c3ea33874fb9be1d84bbc6
Iain Lane <iain@orangesquash.org.uk>
2023-06-17 19:10:41 +0100

    Fix symlink path for install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,8 @@
 # Ansible is used instead.
 
 SCRIPTDIR=$(cd "$(dirname "$0")" && pwd -P)
+ROOT="$(realpath "${SCRIPTDIR}/..")"
 
 # Install the dotfiles.
-ln -s "${SCRIPTDIR}/roles/user/templates/zshrc" "${HOME}/.zshrc"
-ln -s "${SCRIPTDIR}/roles/user/templates/zshrc.local" "${HOME}/.zshrc"
+ln -s "${ROOT}/roles/user/templates/zshrc" "${HOME}/.zshrc"
+ln -s "${ROOT}/roles/user/templates/zshrc.local" "${HOME}/.zshrc"
