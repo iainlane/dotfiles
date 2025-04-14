5303d4f4abcd5c0180a088cdf970f2d424f5960b
Iain Lane <iain@orangesquash.org.uk>
2025-04-14 20:04:14 +0100

    feat(nvim): add snacks explorer explicitly
    I somehow ended up with neotree...

--- roles/user/templates/nvim/lua/config/lazy.lua
+++ roles/user/templates/nvim/lua/config/lazy.lua
@@ -50,6 +50,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
+    { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
