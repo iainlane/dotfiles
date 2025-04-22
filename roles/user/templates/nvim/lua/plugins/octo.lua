406fa4805da014e4f6249b4ede11909aa3d1d4d0
Iain Lane <iain@orangesquash.org.uk>
2025-04-22 14:48:57 +0100

    feat(nvim): configure Octo.nvim with a "my review requests" command
    Not supported natively but we managed to do it using the Lua API.

---
+++ roles/user/templates/nvim/lua/plugins/octo.lua
@@ -0,0 +1,22 @@
+local function get_my_review_requests()
+  local octo_utils = require("octo.utils")
+
+  local repo = octo_utils.get_remote_name()
+
+  local octo_commands = require("octo.commands")
+
+  octo_commands.search("is:open", "is:pr", "review-requested:@me", "repo:" .. repo, "sort:created-desc")
+end
+
+return {
+  "pwntester/octo.nvim",
+
+  keys = {
+    {
+      "<leader>gr",
+      get_my_review_requests,
+      desc = "List My Review Requests (Octo)",
+    },
+    { "<leader>gR", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
+  },
+}
