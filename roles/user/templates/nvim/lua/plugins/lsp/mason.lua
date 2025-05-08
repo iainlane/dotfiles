d02aff88e59b3ea31eec1eb25d940016c00e2d1d
Iain Lane <iain@orangesquash.org.uk>
2025-05-08 12:39:41 +0100

    fix(nvim): pin mason to 1.0.0
    There's breakage in v2 - stick with v1 until LazyVim is fixed.

--- roles/user/templates/nvim/lua/plugins/lsp/mason.lua
+++ roles/user/templates/nvim/lua/plugins/lsp/mason.lua
@@ -1,29 +1,35 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 
-  opts = {
-    ensure_installed = {
-      "css-lsp",
-      "emmet-language-server",
-      "eslint-lsp",
-      "golangci-lint",
-      "gopls",
-      "htmlbeautifier",
-      "jsonnet-language-server",
-      "jsonnetfmt",
-      "lua-language-server",
-      "markdownlint",
-      "prettierd",
-      "prisma-language-server",
-      "pyright",
-      "rust-analyzer",
-      "sorbet", -- ruby
-      "svelte-language-server",
-      "tailwindcss-language-server",
-      "typescript-language-server",
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+
+    opts = {
+      ensure_installed = {
+        "css-lsp",
+        "emmet-language-server",
+        "eslint-lsp",
+        "gh-actions-language-server",
+        "golangci-lint",
+        "gopls",
+        "htmlbeautifier",
+        "jsonnet-language-server",
+        "jsonnetfmt",
+        "lua-language-server",
+        "markdownlint",
+        "prettierd",
+        "prisma-language-server",
+        "pyright",
+        "rust-analyzer",
+        "sorbet", -- ruby
+        "svelte-language-server",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      },
     },
   },
 }
