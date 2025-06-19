23c426afc3172531fc18be12b4ae93892d85f845
Iain Lane <iain@orangesquash.org.uk>
2025-06-19 07:03:44 +0100

    feat(nvim): add overseer plugin
    This lets you run tasks in Neovim, like "run all tests".

--- roles/user/templates/nvim/lua/config/lazy.lua
+++ roles/user/templates/nvim/lua/config/lazy.lua
@@ -49,6 +49,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
+    { import = "lazyvim.plugins.extras.editor.overseer" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
