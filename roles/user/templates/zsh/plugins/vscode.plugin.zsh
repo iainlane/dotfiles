f73da2c8cb6ffaf43db3f07bea8fbd5b2bbc854b
Iain Lane <iain@orangesquash.org.uk>
2025-06-23 13:26:03 +0100

    feat(zsh/vscode): support the insiders shell integration
    We call `code` to source the right shell integration script. When
    running the insiders build, we need to call `code-insiders`. There's a
    variable `${TERM_PROGRAM_VERSION}` which has `-insider` appended when
    the terminal is running inside VS Code Insiders.

--- roles/user/templates/zsh/plugins/vscode.plugin.zsh
+++ roles/user/templates/zsh/plugins/vscode.plugin.zsh
@@ -1,1 +1,9 @@
-[[ "${TERM_PROGRAM}" == "vscode" ]] && . "$(code --locate-shell-integration-path zsh)"
+CODE="code"
+
+if [[ "${TERM_PROGRAM_VERSION}" == "*insider" ]]; then
+  CODE="code-insiders"
+fi
+
+if [[ "${TERM_PROGRAM}" == "vscode" ]]; then
+  . "$(${CODE} --locate-shell-integration-path zsh)"
+fi
