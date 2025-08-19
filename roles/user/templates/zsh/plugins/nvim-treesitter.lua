564372a19d0f69e9dd6d765e22c4b54acbadd49a
Iain Lane <iain@grafana.com>
2025-08-19 09:14:28 +0100

    fix: remove stray nvim-treesitter.lua file from the zsh plugins
    Must have accidentally put that there

--- roles/user/templates/zsh/plugins/nvim-treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-
-	opts = function(_, _)
-		-- use bash syntax highlighting for zsh files in the absence of dedicated
-		-- zsh support
-		vim.treesitter.language.register("bash", "zsh")
-	end,
-}
