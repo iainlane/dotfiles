8a0bb7c7cdc9a2896c96ab7d961d91963162d8fd
Iain Lane <iain@orangesquash.org.uk>
2025-09-11 21:06:27 +0100

    feat(nvim): run the jsonnet language server with --tanka
    When we detect we're in a Tanka project (done by running `tk tool jpath`
    on the current buffer).

--- roles/user/templates/nvim/lua/plugins/jsonnet.lua
+++ roles/user/templates/nvim/lua/plugins/jsonnet.lua
@@ -28,17 +28,44 @@ return {
 
     opts = {
       servers = {
-        jsonnet_ls = {
-          flags = {
-            debounce_text_changes = 150,
-          },
-          cmd = { "jsonnet-language-server", "--lint" },
-          settings = {
-            formatting = {
-              UseImplicitPlus = true,
-            },
-          },
-        },
+        ---@type lspconfig.options
+        jsonnet_ls = (function()
+          ---Run a command and return true if it exits with success
+          ---@param args string[]  e.g. { "tk", "tool", "jpath", "/abs/file.jsonnet" }
+          ---@param cwd  string|nil
+          ---@return boolean ok
+          local function cmd_ok(args, cwd)
+            local res = vim.system(args, { cwd = cwd }):wait()
+            if not res then
+              return false
+            end
+            return res.code == 0
+          end
+
+          ---@class JsonnetClientConfig: vim.lsp.ClientConfig
+          ---@field cmd string[]
+
+          return {
+            flags = { debounce_text_changes = 150 },
+            cmd = { "jsonnet-language-server", "--lint" },
+            settings = { formatting = { UseImplicitPlus = true } },
+
+            ---Run the language server with `--tanka` if we're in a Tanka
+            ---project.
+            ---@param new_config JsonnetClientConfig
+            ---@param root_dir string
+            on_new_config = function(new_config, root_dir)
+              local bufnr = vim.api.nvim_get_current_buf()
+              local file = vim.api.nvim_buf_get_name(bufnr)
+
+              if not cmd_ok({ "tk", "tool", "jpath", file }, root_dir) then
+                return
+              end
+
+              new_config.cmd = { "jsonnet-language-server", "--lint", "--tanka" }
+            end,
+          }
+        end)(),
       },
     },
   },
