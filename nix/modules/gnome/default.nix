6081d5da5b237b79bb29d42ae72d9520f275ba33
Iain Lane <iain@orangesquash.org.uk>
2026-03-09 12:25:41 +0000

    feat: add GNOME module

---
+++ nix/modules/gnome/default.nix
@@ -0,0 +1,53 @@
+_: let
+  nixosModule = {
+    lib,
+    pkgs,
+    ...
+  }: {
+    services.xserver = {
+      desktopManager.gnome.enable = true;
+      displayManager.gdm = {
+        enable = true;
+        wayland = true;
+      };
+    };
+
+    programs.dconf.enable = true;
+
+    environment.gnome.excludePackages = with pkgs; [
+      epiphany
+      geary
+      gnome-maps
+      gnome-music
+      gnome-tour
+      totem
+    ];
+  };
+
+  homeManagerModule = {lib, ...}: {
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        clock-format = "24h";
+        color-scheme = "prefer-dark";
+        gtk-theme = "adw-gtk3-dark";
+      };
+      "org/gnome/desktop/peripherals/touchpad" = {
+        tap-to-click = true;
+        natural-scroll = true;
+      };
+      "org/gnome/mutter" = {
+        edge-tiling = true;
+        dynamic-workspaces = true;
+        workspaces-only-on-primary = true;
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,maximize,close";
+      };
+    };
+  };
+in {
+  flake.modules.gnome = {
+    nixosModules = [nixosModule];
+    os.nixos.homeManagerModules = [homeManagerModule];
+  };
+}
