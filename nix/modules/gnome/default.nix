80ab7e0410205887fead095fd49e8f54a622f4b7
Iain Lane <iain.lane@chainguard.dev>
2026-03-17 10:50:51 +0000

    fix(gnome): stop setting explicit themes
    We're using the auto dark theme extension for this. Having the overrides
    for `color-scheme` and `gtk-theme` was causing the dark theme to be
    applied on every activation.

--- nix/modules/gnome/default.nix
+++ nix/modules/gnome/default.nix
@@ -53,9 +53,7 @@ _: let
         };
         "org/gnome/desktop/interface" = {
           clock-format = "24h";
-          color-scheme = "prefer-dark";
           font-antialiasing = "rgba";
-          gtk-theme = "adw-gtk3-dark";
         };
         "org/gnome/desktop/peripherals/touchpad" = {
           tap-to-click = true;
