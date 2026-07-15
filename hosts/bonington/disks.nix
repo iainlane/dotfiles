b6272416bade78cc29e17c8991c7332eed00b282
Iain Lane <iain.lane@chainguard.dev>
2026-07-15 11:42:31 +0100

    feat(bonington): add zram swap, set swapfile priority, and don't restate default mount options

--- hosts/bonington/disks.nix
+++ hosts/bonington/disks.nix
@@ -1,5 +1,9 @@
 let
-  btrfsMountOptions = ["compress=zstd" "discard=async" "noatime" "space_cache=v2" "ssd"];
+  btrfsMountOptions = [
+    "compress=zstd"
+    "noatime"
+  ];
+
   dataSubvolumes =
     builtins.mapAttrs (_: mountpoint: {
       inherit mountpoint;
@@ -15,34 +19,50 @@ in {
     disk.main = {
       type = "disk";
       device = "/dev/disk/by-id/nvme-WD_BLACK_SN7100_1TB_25435D803157";
+
       content = {
         type = "gpt";
+
         partitions = {
           ESP = {
             size = "512M";
             type = "EF00";
+
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = ["umask=0077"];
+              mountOptions = [
+                "umask=0077"
+              ];
             };
           };
+
           luks = {
             size = "100%";
+
             content = {
               type = "luks";
               name = "crypted";
+
               settings.allowDiscards = true;
+
               content = {
                 type = "btrfs";
-                extraArgs = ["-f"];
+                extraArgs = [
+                  "-f"
+                ];
+
                 subvolumes =
                   dataSubvolumes
                   // {
                     "@swap" = {
                       mountpoint = "/.swapvol";
-                      swap.swapfile.size = "8G";
+
+                      swap.swapfile = {
+                        size = "8G";
+                        priority = 10;
+                      };
                     };
                   };
               };
@@ -51,5 +71,12 @@ in {
         };
       };
     };
+  };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 25;
+    priority = 100;
   };
 }
